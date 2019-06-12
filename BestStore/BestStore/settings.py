d51e5789ec3862a4ac060900135cd1ebe5bb4f25
rishabh-22 <rishabh.bh22@gmail.com>
2019-06-12 13:26:39 +0530

    user address model

--- BestStore/BestStore/settings.py
+++ BestStore/BestStore/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'User',
     'Orders',
     'crispy_forms',
+    'django_countries',
 ]
 
 MIDDLEWARE = [
--- BestStore/User/models.py
+++ BestStore/User/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django_countries.fields import CountryField
 
 
 class ContactQuery(models.Model):
@@ -6,3 +8,13 @@ class ContactQuery(models.Model):
     email = models.EmailField(max_length=50, null=True)
     subject = models.CharField(max_length=30, null=True)
     query = models.TextField()
+
+
+class UserAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    landmark = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = CountryField(blank_label='(select country)')
+    mobile = models.IntegerField()
